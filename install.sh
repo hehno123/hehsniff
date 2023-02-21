9320221754b2c3fe77b6d32ce1af201e300485c8
hehno123 <103997606+hehno123@users.noreply.github.com>
2023-02-21 13:11:32 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ hehmitm/*.c -lpcap -lpthread -o /usr/local/bin/hehmitm
-g++ hehsniff/*.c -lpcap -lpthread -o /usr/local/bin/hehsniff
+g++ hehmitm/*.c -lpcap -lpthread -o /usr/local/sbin/hehmitm
+g++ hehsniff/*.c -lpcap -lpthread -o /usr/local/sbin/hehsniff
